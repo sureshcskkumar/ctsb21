d7c82becf0b90adf8b667056c29ad88aab929df9
sureshcskkumar <sureshcskkumar@gmail.com>
2022-04-11 18:18:28 +0530

    task1

--- workspace/p02-springboot-db/src/main/java/com/demo/controllers/MovieController.java
+++ workspace/p02-springboot-db/src/main/java/com/demo/controllers/MovieController.java
@@ -3,8 +3,12 @@ package com.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,4 +33,18 @@ public class MovieController {
 		return service.fetchMovies();
 	}
 	
+	@GetMapping("/movies/{id}")
+	public Movie fetchMovieById(@PathVariable int id) {
+		return service.fetchMovieById(id);
+	}
+	
+	@PutMapping("/movies")
+	public Movie updateMovie(@RequestBody Movie movie) {
+		return service.updateMovie(movie);
+	}
+	
+	@DeleteMapping("/movies/{id}")
+	public ResponseEntity<String> deleteMovieById(@PathVariable int id) {
+		return service.deleteMovieById(id);
+	}
 }
--- workspace/p02-springboot-db/src/main/java/com/demo/services/MovieService.java
+++ workspace/p02-springboot-db/src/main/java/com/demo/services/MovieService.java
@@ -1,8 +1,11 @@
 package com.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.demo.entities.Movie;
@@ -23,5 +26,35 @@ public class MovieService {
 	public List<Movie> fetchMovies() {
 		return repo.findAll();
 	}
-	
+
+	public Movie fetchMovieById(int id) {
+		Optional<Movie> movie = repo.findById(id);
+		if (movie.isPresent()) {
+			return movie.get();
+		}
+		return null;
+	}
+
+	public Movie updateMovie(Movie movie) {
+		if (movie != null) {
+			if (fetchMovieById(movie.getId()) == null) {
+				return null;
+			}
+			else {
+				return repo.save(movie);
+			}
+		}
+		return null;
+	}
+
+	public ResponseEntity<String> deleteMovieById(int id) {
+		Movie movie = fetchMovieById(id);
+		if (movie == null) {
+			return ResponseEntity.notFound().build();
+		}
+		else {
+			repo.delete(movie);
+			return new ResponseEntity<>("Product has been deleted successfully.", HttpStatus.OK);
+		}
+	}
 }
